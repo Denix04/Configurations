2caf88ee610a42b9483d4a101dc108f0538bac38
AlvaroDebernardi <alvarodebernardi1@gmail.com>
2024-09-12 13:52:39 -0300

    Updating the nvim configuration

--- nvim/lua/treesiter_config.lua
+++ nvim/lua/treesiter_config.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup({
-     ensure_installed = { "c", "lua", "latex", "python", "rust", "haskell" },
+     ensure_installed = { "c", "lua", "latex", "python", "rust", "haskell", "java", "bash", "asm", "javascript", "css", "html"},
      sync_install = false,
      auto_install = false,
      highlight = {
